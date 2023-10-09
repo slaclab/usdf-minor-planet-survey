5d3936cd17406ad3ce64cd58b355f1709a7e0608
dspeck1 <dspeck@burwood.com>
2023-10-09 16:02:56 -0500

    Create grants.sql
    Permissions for Rubin

---
+++ kubernetes/overlays/prod/sql/grants.sql
@@ -0,0 +1,15 @@
+GRANT SELECT ON public.current_identifications TO rubin;
+GRANT SELECT ON public.mpc_orbits  TO rubin;
+GRANT SELECT ON public.neocp_els TO rubin;
+GRANT SELECT ON public.neocp_events TO rubin;
+GRANT SELECT ON public.neocp_obs TO rubin;
+GRANT SELECT ON public.neocp_obs_archive TO rubin;
+GRANT SELECT ON public.neocp_prev_des TO rubin;
+GRANT SELECT ON public.neocp_var TO rubin;
+GRANT SELECT ON public.numbered_identifications TO rubin;
+GRANT SELECT ON public.obs_alterations_corrections TO rubin;
+GRANT SELECT ON public.obs_alterations_deletions TO rubin;
+GRANT SELECT ON public.obs_alterations_redesignations TO rubin;
+GRANT SELECT ON public.obs_alterations_unassociations TO rubin;
+GRANT SELECT ON public.obs_sbn TO rubin;
+GRANT SELECT ON public.primary_objects TO rubin;
